143297a1436c9aaac270d7ddf12e3300fd61cedf
pozeary <pozeary@foxmail.com>
2018-09-24 07:57:20 -0700

    add func complete

--- test.c
+++ test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+int add(int x, int y)
+{
+         return x + y;
+}
+ 
 int main()
 {
-     printf("hello, world!\n");
-     return 0;
-}
+	printf("hello, world!\n");
+	printf("1 + 2 %d\n", add(1, 2));
+ 	return 0;
+}		
